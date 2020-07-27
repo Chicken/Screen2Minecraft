13f52924198014aed1ad88e380f5114f22d73289
BuildTools <unconfigured@null.spigotmc.org>
2020-07-27 14:41:06 +0300

    fricc comments

--- recorder/index.js
+++ recorder/index.js
@@ -4,16 +4,10 @@ const net = require("net");
 let client = net.connect(1337, "localhost")
 
 setInterval(()=>{
-    //timing
     let start = Date.now();
-
-    //get screen capture
     let ss = robot.screen.capture();
-
     client.write(ss.image)
-
-    //timing
     let elapsed = Date.now()-start;
-    //logging
-    console.log(`Finished in ${elapsed}ms.`);
+    console.log(`Finished in ${elapsed}ms. Extra data:`);
+    console.log(ss);
 }, 1000)
