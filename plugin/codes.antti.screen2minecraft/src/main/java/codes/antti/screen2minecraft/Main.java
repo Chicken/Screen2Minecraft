1478b71f8619e0131def4e3945f1d61120ad3620
Chicken-E <antti@ellila.fi>
2020-08-01 12:59:53 +0300

    progress on colors

--- plugin/codes.antti.screen2minecraft/src/main/java/codes/antti/screen2minecraft/Main.java
+++ plugin/codes.antti.screen2minecraft/src/main/java/codes/antti/screen2minecraft/Main.java
@@ -8,6 +8,9 @@ import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.World;
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.awt.Color;
 
 public class Main extends JavaPlugin {
     private Thread socketHandler;
@@ -18,6 +21,7 @@ public class Main extends JavaPlugin {
     private int bufferSize;
     private BukkitScheduler scheduler;
     private World world;
+    public HashMap<String, String> blocks = new HashMap<String, String>();
     public int screenWidth;
     public int screenHeight;
     public int downScale;
@@ -30,6 +34,22 @@ public class Main extends JavaPlugin {
         socketHandler = new Thread() {
             public void run() {
                 try {
+                    blocks.put("WHITE_CONCRETE", "CFD5D6");
+                    blocks.put("ORANGE_CONCRETE","E06101");
+                    blocks.put("MAGENTA_CONCRETE","A9309F");
+                    blocks.put("LIGHT_BLUE_CONCRETE","2489C7");
+                    blocks.put("YELLOW_CONCRETE","F1AF15");
+                    blocks.put("LIME_CONCRETE","5EA919");
+                    blocks.put("PINK_CONCRETE","D6658F");
+                    blocks.put("GRAY_CONCRETE","373A3E");
+                    blocks.put("LIGHT_GRAY_CONCRETE","7D7D73");
+                    blocks.put("CYAN_CONCRETE","157788");
+                    blocks.put("PURPLE_CONCRETE","64209C");
+                    blocks.put("BLUE_CONCRETE","2D2F8F");
+                    blocks.put("BROWN_CONCRETE","603C20");
+                    blocks.put("GREEN_CONCRETE","495B24");
+                    blocks.put("RED_CONCRETE","8E2121");
+                    blocks.put("BLACK_CONCRETE","080A0F");
                     scheduler = Bukkit.getServer().getScheduler();
                     world = Bukkit.getServer().getWorld("world");
                     screenWidth = config.getInt("screenWidth");
@@ -49,6 +69,9 @@ public class Main extends JavaPlugin {
                     while (true) {
                         int cur = in.read();
                         if(index != bufferSize-1) {
+                            if(cur>255) {
+                                cur = 255;
+                            }
                             data[index] = cur;
                             index++;
                         } else {
@@ -56,7 +79,7 @@ public class Main extends JavaPlugin {
                                 for(int x = 0; x < screenWidth/downScale; x++) {
                                     Location loc = new Location(world, x,3,y);
                                     int dataIndex = x*bytesPerPixel*downScale + y*screenWidth*bytesPerPixel*downScale;
-                                    Material block = Material.valueOf(findClosest(toHex(data[dataIndex], data[dataIndex+1], data[dataIndex+2])));
+                                    Material block = Material.valueOf(findClosest(data[dataIndex+2], data[dataIndex+1], data[dataIndex]));
                                     scheduler.scheduleSyncDelayedTask(plugin, new Runnable() {
                                         public void run() {
                                             loc.getBlock().setType(block);
@@ -92,32 +115,32 @@ public class Main extends JavaPlugin {
         getLogger().info("Screen to minecraft has been disabled.");
     }
 
-    public String findClosest(String hex) {
-        int num = Integer.parseInt(hex,16);
-        if(num==16711680) {
-            return "RED_CONCRETE";
-        } 
-        if(num<8421504) {
-            return "BLACK_CONCRETE";
-        } else {
-            return "WHITE_CONCRETE";
+    public String findClosest(int r, int g, int b) {
+        double closestDist = Double.MAX_VALUE;
+        String closestBlock = "";
+        for(Map.Entry<String, String> entry : blocks.entrySet()) {
+            String name = entry.getKey();
+            String value = entry.getValue();
+            double distance = dist(new Color(r, g, b), Color.decode("#"+value));
+            if(distance<closestDist) {
+                closestDist = distance;
+                closestBlock = name; 
+            }    
         }
+        return closestBlock;
     }
 
-    public String toHex(int r ,int g ,int b) {
-        String hex = "";
-        hex += Integer.toHexString(r);
-        hex += Integer.toHexString(g);
-        hex += Integer.toHexString(b);
-        try {
-            Integer.parseInt(hex,16);
-        } catch (NumberFormatException e) {
-            getLogger().info(String.valueOf(r) + " " + String.valueOf(g) + " " + String.valueOf(b));
-            return "ff0000";
-        }
-        return hex;
-    }
-    public String right(String value, int length) {
-        return value.substring(value.length() - length);
+    public double dist(Color a, Color b) {
+        int r1 = a.getRed();
+        int g1 = a.getGreen();
+        int b1 = a.getBlue();
+        int r2 = b.getRed();
+        int g2 = a.getGreen();
+        int b2 = a.getBlue();
+        double drp2 = Math.pow(r1-r2,2);
+        double dgp2 = Math.pow(g1-g2,2);
+        double dbp2 = Math.pow(b1-b2,2);
+        int t = (r1+r2)/2;
+        return Math.sqrt( 2 * drp2 + 4 * dgp2 + 3 * dbp2 + t * ( drp2 - dbp2 ) / 256);
     }
 }
