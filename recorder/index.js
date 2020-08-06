e3ac81b553aa687f1187d773ce763da903e6905e
Chicken-E <antti@antti.codes>
2020-08-06 17:25:24 +0300

    client side image downscale

--- recorder/index.js
+++ recorder/index.js
@@ -1,18 +1,24 @@
 const robot = require("robotjs"),
       net = require("net"),
       config = require("./config.json"),
+      sharp = require('sharp'),
       client = net.connect(config.port, config.address);
 
-let totalFramesSend = 0;
-
 function sendScreen() {
-    client.write(robot.screen.capture(0,0,config.screenWidth,config.screenHeight).image, ()=>{
-        totalFramesSend++;
-        sendScreen()
-    });
+    sharp(robot.screen.capture(0,0,config.screenWidth,config.screenHeight).image, {
+        raw: {
+            width: config.screenWidth,
+            height: config.screenHeight,
+            channels: 4,
+        }
+    })
+    .removeAlpha()
+    .resize(config.targetWidth, config.targetHeight, {kernel: "nearest"})
+    .toBuffer((err,data,info)=>{
+        client.write(data);
+    })
 }
 
-sendScreen();
 setInterval(()=>{
-    console.log("Fps: ", (totalFramesSend/process.uptime()).toFixed(1));
-}, 1000)
+    sendScreen()
+}, 1000/config.fps)
